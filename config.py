9ee96a716deb261e0bb0ca2e9d56b40b15381a9c
UttU28 <utsavmaan28@gmail.com>
2024-08-24 21:38:21 -0500

    sac

--- config.py
+++ config.py
@@ -18,9 +18,3 @@ class AzureSQLConfig(Config):
         f"Pwd={databasePassword};"
         f"Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
     )
-
-class AzureBLOBConfig(Config):
-    AZURE_CONNECTION_STRING='DefaultEndpointsProtocol=https;AccountName=dicestorage02;AccountKey=OFusrDHbeLjeipF0m836T13AakwgBzaX7gbAl+Cjw46N1K/dEEHvVbiC2Mw+JYE67v+2KNqg7BAN+AStPE7SGw==;EndpointSuffix=core.windows.net'
-    CONTAINER_NAME='resume-data'
-    # AZURE_CONNECTION_STRING = os.getenv('blobConnectionString')
-    # CONTAINER_NAME = os.getenv('resumeContainer')
